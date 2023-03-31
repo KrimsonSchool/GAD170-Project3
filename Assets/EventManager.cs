9fda1fabc1115541ac6306e0868a2761c993bb34
KrimsonSchool <1027859@student.sae.edu.au>
2023-03-31 11:20:25 +1000

    no message

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -5,28 +5,40 @@ using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
-    public static UnityEvent pause; 
+    public UnityEvent pause=new UnityEvent();
+    public UnityEvent unpause=new UnityEvent();
+    bool paused;
     // Start is called before the first frame update
     void Start()
     {
-        if (pause == null)
-        {
-            pause = new UnityEvent();
-        }
         pause.AddListener(Pause);
+        unpause.AddListener(unPause);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !paused)
         {
+            print("Invoke");
             pause?.Invoke();
+
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            unpause?.Invoke();
         }
     }
 
     public void Pause()
     {
+        Time.timeScale = 0;
+        paused = true;
+    }
 
+    public void unPause()
+    {
+        Time.timeScale = 1;
+        paused = false;
     }
 }
