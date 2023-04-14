5dafadbf30114aca3795d46152f73d5ca8af0961
KrimsonSchool <1027859@student.sae.edu.au>
2023-04-14 11:59:07 +1000

    basic gameplay kinda

--- Assets/MyScripts/WorldManager.cs
+++ Assets/MyScripts/WorldManager.cs
@@ -6,6 +6,12 @@ public class WorldManager : MonoBehaviour
 {
     public int[]resource;
     public GameObject[] enemies;
+
+    public GameObject enemyPrefab;
+    public GameObject soldierPrefab;
+
+    public GameObject[] resourcePrefabs;
+    public int noToSpawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,11 @@ public class WorldManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (noToSpawn != 0)
+        {
+            Instantiate(resourcePrefabs[Random.Range(0, 6)], new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
+            enemies[noToSpawn] = Instantiate(enemyPrefab, new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
+            noToSpawn -= 1;
+        }
     }
 }
