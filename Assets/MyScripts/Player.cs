5cc0a534ad3bdeda4b92042c6fc077ec5072cb83
Jai Vanderark <1027859@student.sae.edu.au>
2023-04-29 15:46:32 +1000

    started with sudo code + fixed small bugs

--- Assets/MyScripts/Player.cs
+++ Assets/MyScripts/Player.cs
@@ -29,95 +29,137 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //set the health sliders max to players health
         healthSlider.maxValue = health;
 
+        //make the cursor not visible and locked to the playable window
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
 
+        //variable hp is set to players health
         hp = health;
 
+        //1% chance for an ad to play when the game starts
         if(Random.Range(0, 100) == 1)
         {
             ad.SetActive(true);
         }
+        
+        //rng variable is set to a random value between 1 and 15
         rng = Random.Range(1, 15);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //tmr = tmr + time
         tmr += Time.deltaTime;
 
+        //if tmr is greater or equal to rng variable's value
         if(tmr>=rng)
         {
+            //then set the cursor to be visible
             Cursor.visible = true;
+
+            //show the ad
             ad.SetActive(true);
+
+            //re-randomise rng variable
             rng = Random.Range(1, 15);
+
+            //set tmr to 0
             tmr = 0;
         }
 
+        //if right mouse button down
         if (Input.GetMouseButton(1))
         {
+            //then players right to left position is + horizontal axis key values times by speed variable
             transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+            //player y rotation is set to mouse x axis value
+            transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         }
         else
         {
-            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * speed * 100, 0);
-        }
-        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
-
-        healthSlider.value = health;
-
-        if (Input.GetMouseButton(1))
-        {
-            gameObject.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * (speed * 100), 0);
-        }
-        else
-        {
+            //if not then players y rotation is set to horizontal axis key values times by speed variable times by 50
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * speed * 50, 0);
+            
+            //turret y rotation is set to mouse x axis value
             turret.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         }
 
+        //players forward to backward position is + vertical axis key values times by speed variable 
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        //health slider value is set to health variable value
+        healthSlider.value = health;
+
+        //if Q key is pressed
         if (Input.GetKeyDown(KeyCode.Q))
         {
+            //if not building
             if (!FindObjectOfType<BuildingManager>().building)
             {
+                //then run function enterbuildingmode
                 FindObjectOfType<BuildingManager>().enterBuildingMode();
             }
             else
             {
+                //if not run function exit building mode
                 FindObjectOfType<BuildingManager>().exitBuildingMode();
+
+                //set time scale to be 1
                 Time.timeScale = 1;
             }
         }
 
+        //if health variable does not equal hp variable
         if (health != hp)
         {
+            //then set players material to be injured material
             meshRenderer.material = injuredMat;
             meshRenderer2.material = injuredMat;
 
+            //set hptimer to be hptimer + time
             hpTimer += Time.deltaTime;
         }
 
+        //if hptimer variable is greater or equal to 0.3
         if (hpTimer >= 0.3f)
         {
+            //then set hptimer to be 0
             hpTimer = 0;
+
+            //hp variable equals health variable
             hp = health;
+
+            //set players material to be normal material
             meshRenderer.material = mainMat;
             meshRenderer2.material = mainMat;
         }
 
+        //if health variable is equal or less than 0
         if (health <= 0)
         {
+            //then increase timescale by 0.1
             Time.timeScale += 0.1f;
+
+            //reload game scene
             SceneManager.LoadScene(0);
         }
     }
 
+    //on trigger enter other object
     private void OnTriggerEnter(Collider other)
     {
+        //if other objects tag is "Resource"
         if(other.tag == "Resource")
         {
+            //then give resource of type of other object
             FindObjectOfType<WorldManager>().resource[other.GetComponent<Resource>().type] += 1;
+
+            //delete other object
             Destroy(other.gameObject);
         }
     }
--- Assets/MyScripts/WorldManager.cs
+++ Assets/MyScripts/WorldManager.cs
@@ -25,53 +25,82 @@ public class WorldManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //spawn enemy function
         SpawnEnemy();
+
+        //spawn resoruce function
         SpawnResource();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //is the score greater than the highscore?
         if (PlayerPrefs.GetInt("HighScore") < score)
         {
+            //if yes, set the highscore to the score
             PlayerPrefs.SetInt("HighScore", score);
         }
 
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            SpawnEnemy();
-            SpawnResource();
-        }
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    SpawnEnemy();
+        //    SpawnResource();
+        //}
 
+        //timer = timer + time
         timer += Time.deltaTime;
 
+        //if 90 seconds have passed (1 and a half minutes)
         if (timer >= 90)
         {
+            //then set the amount to spawn at 250
             noToSpawn += 250;
+
+            //spawn enemy function
             SpawnEnemy();
+
+            //spawn resource function
             SpawnResource();
+
+            //set timer to 0
             timer = 0;
         }
 
+        //score text = score
         scoreText.text = "Score: " + score;
+
+        //highscore text = highscore
         highScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
     }
 
+    //spawn enemy function
     public void SpawnEnemy()
     {
+        //loop for amount to spawn
         for (int i = 0; i < noToSpawn; i++)
         {
+            //spawn an enemy positionally between -100 and 100 on both the x and z axis and add the enemy to the enemy list
             enemies[spawned] = Instantiate(enemyPrefab, new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
+
+            //set the enemies speed to the speed variable
             enemies[spawned].GetComponent<enemy>().speed = speed;
+
+            //set the amount that has been spawned to + 1
             spawned += 1;
         }
 
+        //after the round of spawning the speed variable is 1.3 times itself making the enemies harder each round (every 90 seconds)
         speed *= 1.3f;
     }
+
+    //spawn resource function
     public void SpawnResource()
     {
+        //loop for amount to spawn
         for (int i = 0; i < noToSpawn; i++)
         {
+            //spawn a resource positionally between -100 and 100 on both the x and z axis
             Instantiate(resourcePrefabs[Random.Range(0, 6)], new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
         }
     }
